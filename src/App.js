44525efe8e1a210f96859c201dcde2c71ed2bb7b
bluesky <blueksy@naver.com>
2025-09-01 18:04:03 +0900

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div className="App">
+            실습 파일들을 라우팅을 사용하여 포함 시킬 예정
+        </div>
+    );  
 }
 
-export default App;
+export default App ;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+  StrictMode 모드는 개발 도중 발생할 수 있는 문제를 꼼꼼히 감지하기 위하여
+  rendering을 두 번 실행해 줍니다.
+  따라서,  <React.StrictMode>를 삭제하도록 합니다.
+*/
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
