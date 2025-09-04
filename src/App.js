7a6f366016a917af2bcfa8469664c067685e3fa4
bluesky <bluesky@example.com>
2025-09-04 15:39:55 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
 // 특정 페이지로 이동을 하기 위한 링크 
-import {Link} from 'react-router-dom' ;
+import { Link } from 'react-router-dom';
 
 // 개발자가 작성한 외부 모듈의 일부를 임포트 합니다.
 import AppRouters from './routes/AppRoutes';
-import {menuData} from './routes/menuData'
+import { menuData } from './routes/menuData'
 
 // 이 문서의 스타일은 App.css가 담당입니다. 
-import './style/App.css' ;
+import './style/App.css';
 
-function App(){
-    return(
+function App() {
+    return (
         <div className="App">
             <h1>React Example Navigation</h1>
             <table border="1">
-                <tr>
-                    {menuData.map((item) => (
-                        <th>{item.chapter}</th>
-                    ))}
-                </tr>
-                <tr>
-                    {menuData.map((item) => (
-                        <td key={item.chapter}>
-                            <ul>
-                                {item.items.map((bean) =>(
-                                    <li key={bean.path}>
-                                        <Link to={bean.path}>{bean.label}</Link>    
-                                    </li>
-                                ))}
-                            </ul>
-                        </td>
-                    ))}
-                </tr>
+                <thead>
+                    <tr>
+                        {menuData.map((item, index) => (
+                            <th key={index}>{item.chapter}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {menuData.map((item) => (
+                            <td key={item.chapter}>
+                                <ul>
+                                    {item.items.map((bean) => (
+                                        <li key={bean.path}>
+                                            <Link to={bean.path}>{bean.label}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </td>
+                        ))}
+                    </tr>
+                </tbody>
             </table>
 
             {/* 라우터 모음 */}
             <AppRouters />
         </div>
-    );  
+    );
 }
 
-export default App ;
+export default App;
---
+++ src/components/ch03.component/use_props.js
@@ -0,0 +1,25 @@
+import Top from './ui/Top02';
+import Content from './ui/Content02';
+import Bottom from './ui/Bottom02';
+
+function App() {
+    // console.log('샘플');
+    const breadList = ["소금빵", "단팥빵", "샌드위치", "도너스", "소보루빵"];
+    const coffeeList = ["아메리카노", "카페라떼", "콜드브루", "바닐라라떼"];
+
+    return (
+        <div className="App">
+            <Top greeting="포레스트 다방" welcome="어서오세요. 우리 매장에서 잼있는 시간 되시길 바랍니다." />
+
+            {/* <Content bread01="소금빵" bread02="단팥빵" bread03="샌드위치" bread04="도너스" /> */}
+
+            {/* breadList를 props로 전달하기 */}
+            <Content type="ul" menus={breadList} />
+            <hr />
+            <Content type="ol" menus={coffeeList} />
+            <Bottom goodbye="안녕히 가세요." comment="다음 번에 또 뵐수 있으면 좋겠습니다." />
+        </div>
+    );
+}
+
+export default App;
