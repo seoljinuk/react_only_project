cebd1551ae6728af6af59af7c765b5611f8dcf5f
bluesky <bluesky@example.com>
2025-09-09 17:01:53 +0900

    hook test

--- src/routes/menuData.js
+++ src/routes/menuData.js
@@ -54,6 +54,8 @@ export const menuData = [
             { path: '/use_state_03', label: '스테이트 실습 03' },
             { path: '/use_state_04', label: '스테이트 실습 04' },
             { path: '/use_effect_01', label: '유저 이펙트 01' },
+            { path: '/use_effect_02', label: '유저 이펙트 02' },
+            { path: '/use_effect_03', label: '유저 이펙트 03' },
         ]
     },
     // 여기에 4장, 5장... 내용을 계속 추가하면 됩니다. 
